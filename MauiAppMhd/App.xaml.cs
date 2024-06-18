35586dbbbf1a7e3a1e69597cac35d3bcdf2486c4
hasalpcode <hasalpcode784@gmail.com>
2024-06-18 10:57:18 +0000

    first commit

---
+++ MauiAppMhd/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMhd
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiAppMhd/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppMhd
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
